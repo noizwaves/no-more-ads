236c6b1c7d73dce94aa1585e4409df9185ff64b9
Adam Neumann <noizwaves@users.noreply.github.com>
2019-07-27 14:48:36 -0600

    Use popular known list of domains as blocklist

--- README.md
+++ README.md
@@ -6,9 +6,11 @@ Ad blocker extension for Chrome, optimised for improving the viewing experience 
 
 -  [Elm 0.19](https://github.com/elm/compiler/releases/tag/0.19.0) installed and available in PATH
 -  [`elm-format` 0.8.1](https://github.com/avh4/elm-format/releases/tag/0.8.1) installed and available in PATH
+-  [StevenBlack's hosts file](https://github.com/StevenBlack/hosts/blob/master/data/StevenBlack/hosts)
 
 ## Getting Started
 
+1.  `wget -o blocklists/hosts https://github.com/StevenBlack/hosts/blob/master/data/StevenBlack/hosts`
 1.  `cd app` and run `./build.sh`
 1.  Load `./manifest.json` as an [unpacked extension](https://developer.chrome.com/extensions/getstarted#manifest) in Chrome
 1.  Visit [Daily Camera](https://www.dailycamera.com)
--- background.js
+++ background.js
@@ -1,50 +1,25 @@
-const blacklist = [
-    // Ads
-    "://ade.googlesyndication.com/",
-    "://tpc.googlesyndication.com/",
-    "://quickresource.eyereturn.com/",
-    "://static.criteo.net/",
-    "://pix.va.us.criteo.net/img/img",
-    "://stackadapt_public.s3.amazonaws.com/",
-    "://cdn.stackadapt.com/",
-    "://widgets.outbrain.com/",
-    "://images.outbrainimg.com/",
-    "://libs.outbrain.com/",
+/* Functions */
 
-    // Tracking
-    "://sb.scorecardresearch.com/",
-    "://asset.pagefair.com/",
-    "://pixel.wp.com/",
-    "://www.google-analytics.com/collect",
-    "://www.google.ca/ads/ga-audiences",
-    "://jadserve.postrelease.com",
-    "://pixel.quantserve.com/",
-    "://ad.doubleclick.net",
-    "://www.facebook.com/tr/",
-    "://securepubads.g.doubleclick.net/",
-    "://www.google.com/ads/measurement/l",
-    "://pagead2.googlesyndication.com/",
-    "://bid.g.doubleclick.net/xbbe/pixel",
-    "://cat.va.us.criteo.com/delivery/lg.php",
-    "://cm.adgrx.com/",
-    "://match.prod.bidr.io/",
-    "://csm2waycm-atl.netmng.com",
-    "://bcp.crwdcntrl.net/",
-    "://dpm.demdex.net/",
-    "://srv.stackadapt.com/",
-    "://evm2.stackadapt.com/",
-    "://aax-us-east.amazon-adsystem.com/",
-    "://dsum-sec.casalemedia.com/",
-    "://log.outbrainimg.com/",
-    "://odb.outbrain.com/",
-    "://mcdp-sadc1.outbrain.com",
-    "://amplify-imp.outbrain.com",
-    "://sync.outbrain.com/"
-];
+const loadBlocklist = function (url, domains) {
+    fetch(url)
+        .then((response) => response.text())
+        .then((body) => {
+            const lines = body.split("\n");
+            lines.forEach(line => {
+                if (line.startsWith("0.0.0.0 ")) {
+                    const domain = line.substr("0.0.0.0 ".length);
+                    if (domain !== "0.0.0.0") {
+                        domains.push(domain.trim());
+                    }
+                }
+            });
+        });
+}
 
 const isBlocked = function (details) {
     let cancel = false;
-    blacklist.forEach(function (entry) {
+    domains.forEach(function (domain) {
+        const entry = "://" + domain;
         cancel = cancel || details.url.indexOf(entry) != -1;
     });
     return cancel
@@ -61,6 +36,16 @@ const addToStore = function (message) {
 const clearStore = function () {
     chrome.storage.local.set({blockedRequests: []});
 };
+
+
+/* State */
+
+const domains = [];
+
+
+/* Main */
+
+loadBlocklist(chrome.runtime.getURL("blocklists/hosts"), domains);
 
 chrome.runtime.onInstalled.addListener(clearStore);
 
